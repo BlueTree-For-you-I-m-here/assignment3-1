31f2765a5ce3646a9e149196d797d06e041d94f0
hirotaka aoki <hirotaka.aoki27@gmail.com>
2020-07-13 06:30:40 +0900

    キャメルケースに統一及びaとbを何を指しているかを明確にしました。

--- main.js
+++ main.js
@@ -29,23 +29,23 @@
          const tr = document.createElement('tr');
          const idTd = document.createElement('td');
          const commentTd = document.createElement('td');
-         const conditionTd_a = document.createElement('td');
-         const conditionTd_b = document.createElement('td');
-         //conditionTd_aにclassを付与
-         conditionTd_a.className = 'btn';
+         const conditionWorkingTd = document.createElement('td');
+         const conditionDeleteTd = document.createElement('td');
+         //conditionWorkingTdにclassを付与
+         conditionWorkingTd.className = 'btn';
          
-         //conditionTd_bにclassを付与
-         conditionTd_b.className = 'btn';
+         //conditionDeleteTdにclassを付与
+         conditionDeleteTd.className = 'btn';
          
          addTaskTarget.appendChild(tr);
          tr.appendChild(idTd); // 1つめ
          idTd.textContent = tasks[i].id;
          tr.appendChild(commentTd); // 2つ目
          commentTd.textContent = tasks[i].comment; // タスク入力値
-         tr.appendChild(conditionTd_a); // 3つ目
-         conditionTd_a.textContent = tasks[i].condition_a;
-         tr.appendChild(conditionTd_b); // 4つ目
-         conditionTd_b.textContent = tasks[i].condition_b;
+         tr.appendChild(conditionWorkingTd); // 3つ目
+         conditionWorkingTd.textContent = tasks[i].condition_a;
+         tr.appendChild(conditionDeleteTd); // 4つ目
+         conditionDeleteTd.textContent = tasks[i].condition_b;
          tasks = [];
       }
    };
